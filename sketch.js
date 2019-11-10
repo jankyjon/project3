e06c47d1e0830a61cc20beabe0c8dfe7b6d2da9c
Jonathan Sjelin <jsjelin@horizon.csueastbay.edu>
2019-11-09 21:37:15 -0800

    added day data

--- sketch.js
+++ sketch.js
@@ -1,34 +1,88 @@
 var policeData;
-var train1min, train2min, train3min;  // asssign variables
+var sunday, monday, tuesday, wednesday, thursday, friday, saturday; // asssign variables
 var url;
+
+function preload() {
+  wordData = loadJSON("https://data.sfgov.org/resource/wg3w-h783.json?incident_category=Larceny%20Theft");
+
+}
 
 function setup() {
   // put setup code here
-  createCanvas(800,1000);
-  background(100);
+  createCanvas(1200, 500);
+  background(200);
 
-url = "https://data.sfgov.org/resource/wg3w-h783.json?incident_category=Larceny%20Theft";
-getPoliceInfo();
+  url = "https://data.sfgov.org/resource/wg3w-h783.json?incident_category=Larceny%20Theft";
+  getPoliceInfo();
+
+
+  // put variables at 0 to start
+  monday = 0;
+  tuesday = 0;
+  wednesday = 0;
+  thursday = 0;
+  friday = 0;
+  saturday = 0;
+  sunday = 0;
+
+
+
+  for (var i = 0; i < 1000; i++) { // the .length command will count like  human like - used to step through
+    var theday = wordData[i].incident_day_of_week; //.substring(0,1); // look up how to use substring
+    if (theday == "Monday") {
+      monday += 1;
+    }
+    if (theday == "Tuesday") {
+      tuesday += 1;
+    }
+    if (theday == "Wednesday") {
+      wednesday += 1;
+    }
+    if (theday == "Thursday") {
+      thursday += 1;
+    }
+    if (theday == "Friday") {
+      friday += 1;
+    }
+    if (theday == "Saturday") {
+      saturday += 1;
+    }
+    if (theday == "Sunday") {
+      sunday += 1;
+    }
+  }
+
+print(sunday + ' sunday')
+print(monday+ ' monday')
+print(tuesday+ ' tuesday')
+print(wednesday+ ' wednesday')
+print(thursday+ ' thurday')
+print(friday+ ' friday')
+print(saturday+ ' saturday')
 
 }
 
-function getPoliceInfo(){
-loadJSON(url, gotData)    // remember - got data is a function
+function getPoliceInfo() {
+  loadJSON(url, gotData) // remember - got data is a function
 }
 
-function gotData(data){
-policeData=data
+function gotData(data) {
+  policeData = data
+
 
 }
 
 function draw() {
   // put drawing code here
+
+
+
 }
 
 
 
 
-function mousePressed(){
-// define button and then call getPoliceInfo()
+function mousePressed() {
+  // define button and then call getPoliceInfo()
 
 }
