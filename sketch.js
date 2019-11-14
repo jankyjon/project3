5c1cc2eeb1facfce60b005912fd7627fb47c9ca5
Matthew Weissman <mweissman@horizon.csueastbay.edu>
2019-11-14 12:45:23 -0800

    Aight, this is wassup.
    This is wassup.

--- sketch.js
+++ sketch.js
@@ -42,7 +42,7 @@ function preload() {
 
 function setup() {
   // put setup code here
-  createCanvas(1200, 500);
+  createCanvas(1200, 600);
   // background(200);
 
   catSlot[0] = "vehicle";
@@ -163,7 +163,7 @@ function getVal(category) {
 // Classic Mode Functions
 function runClassic() {
   background(255);
-  image(b1, 0, 0, 1200, 500);
+  image(b1, 0, 0);
 
   url = "https://data.sfgov.org/resource/wg3w-h783.json?incident_category=Larceny%20Theft";
   getPoliceInfo();
@@ -280,13 +280,13 @@ function supercircles(quantity, verticalPlacement) { // this is so we can easily
 
   for (var i = 0; i < biggy; i++) {
     for (var j = 0; j < 4; j++) {
-      circle(730 + (i * 10), verticalPlacement + (j * 10), 8); // x, y, diameter
+      circle(710 + (i * 10), verticalPlacement + (j * 10), 8); // x, y, diameter
     }
   }
 
   if (smally != 0) {
     for (var i = 0; i < smally; i++){
-      circle(730 + (biggy * 10), verticalPlacement + 30 - (i * 10), 8); // x, y, diameter
+      circle(710 + (biggy * 10), verticalPlacement + 30 - (i * 10), 8); // x, y, diameter
     }
   }
 }
@@ -295,7 +295,7 @@ function supercircles(quantity, verticalPlacement) { // this is so we can easily
 // Time of Day mode functions
 function runTimeOfDay() {
   background(255);
-  image(b1, 0, 0, 1200, 500);
+  image(b1, 0, 0);
 
   url = "https://data.sfgov.org/resource/wg3w-h783.json?incident_category=Larceny%20Theft";
   getPoliceInfo();
@@ -417,14 +417,31 @@ function runTimeOfDay() {
   var sunV = sunT[0] + sunT[1] + sunT[2] + sunT[3] + sunT[4] + sunT[5];
   print('Sunday: ' + sunV);
   var total = monV + tueV + wedV + thuV + friV + satV + sunV;
-  print('Total: ' + total);
+  print('Days Total: ' + total);
 
+
+  textAlign(LEFT);
+  textSize(16);
   fill(0);
+  stroke(0);
+  strokeWeight(0.8);
+  text('Monday', 612, 20);
+  text('Tuesday', 612, 80);
+  text('Wednesday', 612, 155);
+  text('Thursday', 612, 230);
+  text('Friday', 612, 305);
+  text('Saturday', 612, 380);
+  text('Sunday', 612, 455);
+
+  //fill(0);
 
   //subcategory(subcategory,variable)
   vehicle = subcategory("Larceny - From Vehicle");
+  vehicle += subcategory("Theft From Vehicle");
   building = subcategory("Larceny Theft - From Building");
   other = subcategory("Larceny Theft - Other");
+  other += subcategory("Larceny - Auto Parts");
+  other += subcategory("Larceny Theft - Pickpocket");
   shoplift = subcategory("Larceny Theft - Shoplifting");
   bike = subcategory("Larceny Theft - Bicycle");
 
@@ -435,7 +452,7 @@ function runTimeOfDay() {
     var theTime = int(str(wordData[i].incident_time).substring(0,2));
 
     var theCategory = wordData[i].incident_subcategory; //.substring(0,1); // look up how to use substring
-    if (theCategory == "Larceny Theft - From Vehicle") {
+    if (theCategory == "Larceny - From Vehicle") {
       if (theTime < 6) {vehT[0]++;}
       if (theTime >= 6 && theTime < 9) {vehT[1]++;}
       if (theTime >= 9 && theTime < 12) {vehT[2]++;}
@@ -443,7 +460,15 @@ function runTimeOfDay() {
       if (theTime >= 17 && theTime < 20) {vehT[4]++;}
       if (theTime >= 20 && theTime < 24) {vehT[5]++;}
     }
-    else if (theday == "Larceny Theft - From Building") {
+    else if (theCategory == "Theft From Vehicle") {
+      if (theTime < 6) {vehT[0]++;}
+      if (theTime >= 6 && theTime < 9) {vehT[1]++;}
+      if (theTime >= 9 && theTime < 12) {vehT[2]++;}
+      if (theTime >= 12 && theTime < 17) {vehT[3]++;}
+      if (theTime >= 17 && theTime < 20) {vehT[4]++;}
+      if (theTime >= 20 && theTime < 24) {vehT[5]++;}
+    }
+    else if (theCategory == "Larceny Theft - From Building") {
       if (theTime < 6) {buiT[0]++;}
       if (theTime >= 6 && theTime < 9) {buiT[1]++;}
       if (theTime >= 9 && theTime < 12) {buiT[2]++;}
@@ -451,7 +476,7 @@ function runTimeOfDay() {
       if (theTime >= 17 && theTime < 20) {buiT[4]++;}
       if (theTime >= 20 && theTime < 24) {buiT[5]++;}
     }
-    else if (theday == "Larceny Theft - Other") {
+    else if (theCategory == "Larceny Theft - Other") {
       if (theTime < 6) {othT[0]++;}
       if (theTime >= 6 && theTime < 9) {othT[1]++;}
       if (theTime >= 9 && theTime < 12) {othT[2]++;}
@@ -459,7 +484,23 @@ function runTimeOfDay() {
       if (theTime >= 17 && theTime < 20) {othT[4]++;}
       if (theTime >= 20 && theTime < 24) {othT[5]++;}
     }
-    else if (theday == "Larceny Theft - Shoplifting") {
+    else if (theCategory == "Larceny - Auto Parts") {
+      if (theTime < 6) {othT[0]++;}
+      if (theTime >= 6 && theTime < 9) {othT[1]++;}
+      if (theTime >= 9 && theTime < 12) {othT[2]++;}
+      if (theTime >= 12 && theTime < 17) {othT[3]++;}
+      if (theTime >= 17 && theTime < 20) {othT[4]++;}
+      if (theTime >= 20 && theTime < 24) {othT[5]++;}
+    }
+    else if (theCategory == "Larceny Theft - Pickpocket") {
+      if (theTime < 6) {othT[0]++;}
+      if (theTime >= 6 && theTime < 9) {othT[1]++;}
+      if (theTime >= 9 && theTime < 12) {othT[2]++;}
+      if (theTime >= 12 && theTime < 17) {othT[3]++;}
+      if (theTime >= 17 && theTime < 20) {othT[4]++;}
+      if (theTime >= 20 && theTime < 24) {othT[5]++;}
+    }
+    else if (theCategory == "Larceny Theft - Shoplifting") {
       if (theTime < 6) {shoT[0]++;}
       if (theTime >= 6 && theTime < 9) {shoT[1]++;}
       if (theTime >= 9 && theTime < 12) {shoT[2]++;}
@@ -467,7 +508,7 @@ function runTimeOfDay() {
       if (theTime >= 17 && theTime < 20) {shoT[4]++;}
       if (theTime >= 20 && theTime < 24) {shoT[5]++;}
     }
-    else if (theday == "Larceny Theft - Bicycle") {
+    else if (theCategory == "Larceny Theft - Bicycle") {
       if (theTime < 6) {bikT[0]++;}
       if (theTime >= 6 && theTime < 9) {bikT[1]++;}
       if (theTime >= 9 && theTime < 12) {bikT[2]++;}
@@ -480,8 +521,24 @@ function runTimeOfDay() {
   orderCats();
 
   for (var i = 0; i < 5; i++) {
-    drawCrimeCat(i);
+    drawCrimeCatTime(i);
   }
+
+  var vehV = vehT[0] + vehT[1] + vehT[2] + vehT[3] + vehT[4] + vehT[5];
+  print('Vehicle: ' + monV);
+  var buiV = buiT[0] + buiT[1] + buiT[2] + buiT[3] + buiT[4] + buiT[5];
+  print('Building: ' + buiV);
+  var othV = othT[0] + othT[1] + othT[2] + othT[3] + othT[4] + othT[5];
+  print('Other: ' + othV);
+  var shoV = shoT[0] + shoT[1] + shoT[2] + shoT[3] + shoT[4] + shoT[5];
+  print('Shoplift: ' + shoV);
+  var bikV = bikT[0] + bikT[1] + bikT[2] + bikT[3] + bikT[4] + bikT[5];
+  print('Bike: ' + bikV);
+  var totalC = vehV + buiV + othV + shoV + bikV;
+  print('Total Categories: ' + totalC);
+  var totalCA = vehicle + building + other + shoplift + bike;
+  print('Total Categories Alt: ' + totalCA);
+
 
   drawDaysOfWeekTimeOfDay();
 //  drawDaysOfWeekClassic();
@@ -501,6 +558,8 @@ function runTimeOfDay() {
   print(other + ' other');
   print(shoplift + ' shoplifting');
   print(bike + ' bike theft');
+
+  drawLegend();
 }
 
 function drawDaysOfWeekTimeOfDay() {
@@ -513,8 +572,8 @@ function drawDaysOfWeekTimeOfDay() {
   supercirclesTime("sunday", 454);
 }
 
-function supercirclesTime(category, verticalPlacement) { // this is so we can easily create and move circles based on the values
-  if (category == "monday") {
+function supercirclesTime(day, verticalPlacement) { // this is so we can easily create and move circles based on the values
+  if (day == "monday") {
     superCirclesIndividualTime(monday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(monday - int(monT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(monday - int(monT[5]) - int(monT[4]), verticalPlacement, tr3Col);
@@ -522,7 +581,7 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
     superCirclesIndividualTime(int(monT[1]) + int(monT[0]), verticalPlacement, tr1Col);
     superCirclesIndividualTime(int(monT[0]), verticalPlacement, tr0Col);
   }
-  else if (category == "tuesday") {
+  else if (day == "tuesday") {
     superCirclesIndividualTime(tuesday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(tuesday - int(tueT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(tuesday - int(tueT[5]) - int(tueT[4]), verticalPlacement, tr3Col);
@@ -530,7 +589,7 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
     superCirclesIndividualTime(int(tueT[1]) + int(tueT[0]), verticalPlacement, tr1Col);
     superCirclesIndividualTime(int(tueT[0]), verticalPlacement, tr0Col);
   }
-  else if (category == "wednesday") {
+  else if (day == "wednesday") {
     superCirclesIndividualTime(wednesday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(wednesday - int(wedT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(wednesday - int(wedT[5]) - int(wedT[4]), verticalPlacement, tr3Col);
@@ -538,7 +597,7 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
     superCirclesIndividualTime(int(wedT[1]) + int(wedT[0]), verticalPlacement, tr1Col);
     superCirclesIndividualTime(int(wedT[0]), verticalPlacement, tr0Col);
   }
-  else if (category == "thursday") {
+  else if (day == "thursday") {
     superCirclesIndividualTime(thursday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(thursday - int(thuT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(thursday - int(thuT[5]) - int(thuT[4]), verticalPlacement, tr3Col);
@@ -546,7 +605,7 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
     superCirclesIndividualTime(int(thuT[1]) + int(thuT[0]), verticalPlacement, tr1Col);
     superCirclesIndividualTime(int(thuT[0]), verticalPlacement, tr0Col);
   }
-  else if (category == "friday") {
+  else if (day == "friday") {
     superCirclesIndividualTime(friday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(friday - int(friT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(friday - int(friT[5]) - int(friT[4]), verticalPlacement, tr3Col);
@@ -554,7 +613,7 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
     superCirclesIndividualTime(int(friT[1]) + int(friT[0]), verticalPlacement, tr1Col);
     superCirclesIndividualTime(int(friT[0]), verticalPlacement, tr0Col);
   }
-  else if (category == "saturday") {
+  else if (day == "saturday") {
     superCirclesIndividualTime(saturday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(saturday - int(satT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(saturday - int(satT[5]) - int(satT[4]), verticalPlacement, tr3Col);
@@ -562,7 +621,7 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
     superCirclesIndividualTime(int(satT[1]) + int(satT[0]), verticalPlacement, tr1Col);
     superCirclesIndividualTime(int(satT[0]), verticalPlacement, tr0Col);
   }
-  else if (category == "sunday") {
+  else if (day == "sunday") {
     superCirclesIndividualTime(sunday, verticalPlacement, tr5Col);
     superCirclesIndividualTime(sunday - int(sunT[5]), verticalPlacement, tr4Col);
     superCirclesIndividualTime(sunday - int(sunT[5]) - int(sunT[4]), verticalPlacement, tr3Col);
@@ -574,9 +633,9 @@ function supercirclesTime(category, verticalPlacement) { // this is so we can ea
 
 function superCirclesIndividualTime(quantity, verticalPlacement, myCol) {
   noStroke();
-  stroke(0,0.01);
-  strokeWeight(5);
-  fill(70);
+//  stroke(0,0.01);
+//  strokeWeight(5);
+  fill(80);
   print(quantity);
   oompaloompa += quantity;
 
@@ -586,13 +645,13 @@ function superCirclesIndividualTime(quantity, verticalPlacement, myCol) {
   // Overwriting Existing Circles
   for (var i = 0; i < biggy; i++) {
     for (var j = 0; j < 4; j++) {
-      circle(730 + (i * 10), verticalPlacement + (j * 10), 9); // x, y, diameter
+      circle(710 + (i * 10), verticalPlacement + (j * 10), 9.25); // x, y, diameter
     }
   }
 
   if (smally != 0) {
     for (var i = 0; i < smally; i++){
-      circle(730 + (biggy * 10), verticalPlacement + 30 - (i * 10), 9); // x, y, diameter
+      circle(710 + (biggy * 10), verticalPlacement + 30 - (i * 10), 9.25); // x, y, diameter
     }
   }
 
@@ -600,13 +659,92 @@ function superCirclesIndividualTime(quantity, verticalPlacement, myCol) {
   // Making New Circles
   for (var i = 0; i < biggy; i++) {
     for (var j = 0; j < 4; j++) {
-      circle(730 + (i * 10), verticalPlacement + (j * 10), 8); // x, y, diameter
+      circle(710 + (i * 10), verticalPlacement + (j * 10), 8); // x, y, diameter
     }
   }
 
   if (smally != 0) {
     for (var i = 0; i < smally; i++){
-      circle(730 + (biggy * 10), verticalPlacement + 30 - (i * 10), 8); // x, y, diameter
+      circle(710 + (biggy * 10), verticalPlacement + 30 - (i * 10), 8); // x, y, diameter
+    }
+  }
+}
+
+function subcirclesTime(category, horizontalPlacement) {
+  if (category == "vehicle") {
+    subCirclesIndividualTime(vehicle, horizontalPlacement, tr5Col);
+    subCirclesIndividualTime(vehicle - int(vehT[5]), horizontalPlacement, tr4Col);
+    subCirclesIndividualTime(vehicle - int(vehT[5]) - int(vehT[4]), horizontalPlacement, tr3Col);
+    subCirclesIndividualTime(int(vehT[2]) + int(vehT[1]) + int(vehT[0]), horizontalPlacement, tr2Col);
+    subCirclesIndividualTime(int(vehT[1]) + int(vehT[0]), horizontalPlacement, tr1Col);
+    subCirclesIndividualTime(int(vehT[0]), horizontalPlacement, tr0Col);
+  }
+  else if (category == "building") {
+    subCirclesIndividualTime(building, horizontalPlacement, tr5Col);
+    subCirclesIndividualTime(building - int(buiT[5]), horizontalPlacement, tr4Col);
+    subCirclesIndividualTime(building - int(buiT[5]) - int(buiT[4]), horizontalPlacement, tr3Col);
+    subCirclesIndividualTime(int(buiT[2]) + int(buiT[1]) + int(buiT[0]), horizontalPlacement, tr2Col);
+    subCirclesIndividualTime(int(buiT[1]) + int(buiT[0]), horizontalPlacement, tr1Col);
+    subCirclesIndividualTime(int(buiT[0]), horizontalPlacement, tr0Col);
+  }
+  else if (category == "other") {
+    subCirclesIndividualTime(other, horizontalPlacement, tr5Col);
+    subCirclesIndividualTime(other - int(othT[5]), horizontalPlacement, tr4Col);
+    subCirclesIndividualTime(other - int(othT[5]) - int(othT[4]), horizontalPlacement, tr3Col);
+    subCirclesIndividualTime(int(othT[2]) + int(othT[1]) + int(othT[0]), horizontalPlacement, tr2Col);
+    subCirclesIndividualTime(int(othT[1]) + int(othT[0]), horizontalPlacement, tr1Col);
+    subCirclesIndividualTime(int(othT[0]), horizontalPlacement, tr0Col);
+  }
+  else if (category == "shoplift") {
+    subCirclesIndividualTime(shoplift, horizontalPlacement, tr5Col);
+    subCirclesIndividualTime(shoplift - int(shoT[5]), horizontalPlacement, tr4Col);
+    subCirclesIndividualTime(shoplift - int(shoT[5]) - int(shoT[4]), horizontalPlacement, tr3Col);
+    subCirclesIndividualTime(int(shoT[2]) + int(shoT[1]) + int(shoT[0]), horizontalPlacement, tr2Col);
+    subCirclesIndividualTime(int(shoT[1]) + int(shoT[0]), horizontalPlacement, tr1Col);
+    subCirclesIndividualTime(int(shoT[0]), horizontalPlacement, tr0Col);
+  }
+  else if (category == "bike") {
+    subCirclesIndividualTime(bike, horizontalPlacement, tr5Col);
+    subCirclesIndividualTime(bike - int(bikT[5]), horizontalPlacement, tr4Col);
+    subCirclesIndividualTime(bike - int(bikT[5]) - int(bikT[4]), horizontalPlacement, tr3Col);
+    subCirclesIndividualTime(int(bikT[2]) + int(bikT[1]) + int(bikT[0]), horizontalPlacement, tr2Col);
+    subCirclesIndividualTime(int(bikT[1]) + int(bikT[0]), horizontalPlacement, tr1Col);
+    subCirclesIndividualTime(int(bikT[0]), horizontalPlacement, tr0Col);
+  }
+}
+
+function subCirclesIndividualTime(quantity, horizontalPlacement, myCol) { // this is so we can easily create and move circles based on the values
+  noStroke();
+
+  var biggy = int(quantity / 12);
+  var smally = quantity % 12;
+
+  fill(80);
+
+  // overwriting existing circles
+  for (var i = 0; i < biggy; i++) {
+    for (var j = 0; j < 12; j++) {
+      circle(horizontalPlacement + (j * 8), 460 - (i * 8), 7); // x, y, diameter
+    }
+  }
+
+  if (smally != 0) {
+    for (var i = 0; i < smally; i++){
+      circle(horizontalPlacement + 88 - (i * 8), 460 - (biggy * 8), 7); // x, y, diameter
+    }
+  }
+
+  fill(myCol);
+  // creating new circles
+  for (var i = 0; i < biggy; i++) {
+    for (var j = 0; j < 12; j++) {
+      circle(horizontalPlacement + (j * 8), 460 - (i * 8), 6); // x, y, diameter
+    }
+  }
+
+  if (smally != 0) {
+    for (var i = 0; i < smally; i++){
+      circle(horizontalPlacement + 88 - (i * 8), 460 - (biggy * 8), 6); // x, y, diameter
     }
   }
 }
@@ -617,4 +755,102 @@ function getSectionQuantity(category, timeslot) {
   else if (category == "other") {return vehT[timeslot];}
   else if (category == "shoplift") {return vehT[timeslot];}
   else if (category == "bike") {return vehT[timeslot];}
+}
+
+function drawCrimeCatTime(slot) {
+  var tempString;
+
+  if (catSlot[slot] == "building") {
+    tempString = "From Building";
+  }
+  else if (catSlot[slot] == "bike") {
+    tempString = "Of Bicycle";
+  }
+  else if (catSlot[slot] == "other") {
+    tempString = "Other";
+  }
+  else if (catSlot[slot] == "shoplift") {
+    tempString = "Shoplifting";
+  }
+  else if (catSlot[slot] == "vehicle") {
+    tempString = "From Vehicle";
+  }
+
+  textAlign(CENTER);
+  textSize(16);
+  fill(0);
+  stroke(0);
+  strokeWeight(0.8);
+  text(tempString, 49 + (124 * slot), 487);
+  subcirclesTime(catSlot[slot], 5 + (124 * slot));
+}
+
+function drawLegend() {
+  noStroke();
+
+  fill(tr0Col);
+  rectAlt(0, 540, 300, 30);
+
+  fill(tr1Col);
+  rectAlt(300, 540, 150, 30);
+
+  fill(tr2Col);
+  rectAlt(450, 540, 150, 30);
+
+  fill(tr3Col);
+  rectAlt(600, 540, 250, 30);
+
+  fill(tr4Col);
+  rectAlt(850, 540, 150, 30);
+
+  fill(tr5Col);
+  rectAlt(1000, 540, 200, 30);
+
+
+  fill(0);
+  stroke(0);
+  strokeWeight(1);
+  line(xPosMod(0),570,xPosMod(1200),570);
+
+  for (var i = 0; i < 25; i++) {
+    line(xPosMod(i * 50), 560, xPosMod(i * 50), 575);
+  }
+
+  textAlign(LEFT);
+  textSize(16);
+  fill(0);
+  stroke(0);
+  strokeWeight(0.5);
+  text('Legend:', 10, 527);
+
+  textAlign(CENTER);
+  textSize(12);
+  strokeWeight(0);
+  text('Midnight', xPosMod(0), 590);
+  for (var i = 1; i < 12; i++) {
+    text(str(i) + 'am', xPosMod(i*50), 590);
+  }
+  text('Noon', xPosMod(600), 590);
+  for (var i = 13; i < 24; i++) {
+    text(str(i - 12) + 'pm', xPosMod(i*50), 590);
+  }
+  text('Midnight', xPosMod(1200), 590);
+
+  strokeWeight(0.5);
+  text('Midnight', xPosMod(0), 590);
+  text('6am', xPosMod(300), 590);
+  text('9am', xPosMod(450), 590);
+  text('Noon', xPosMod(600), 590);
+  text('5pm', xPosMod(850), 590);
+  text('8pm', xPosMod(1000), 590);
+  text('Midnight', xPosMod(1200), 590);
+}
+
+function rectAlt(xpos, ypos, width, height) {
+  rect(xPosMod(xpos), ypos, width * 0.916666667, height);
+}
+
+function xPosMod(xpos) {
+  var blah = (xpos * 0.916666667) + 50;
+  return blah;
 }
