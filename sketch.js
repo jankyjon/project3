8b955b211b498c702f0adbcde92b28a6e8c3c8f9
Matthew Weissman <mweissman@horizon.csueastbay.edu>
2019-12-10 17:11:58 -0800

    another

--- sketch.js
+++ sketch.js
@@ -107,16 +107,16 @@ function setup() {
 
   // colors for Districts mode:
   d0col = color(180,80,90); // Bayview
-  d1col = color(120,60,90); // Central
-  d2col = color(330,70,95); // Ingleside
-  d3col = color(270,50,90); // Mission
+  d1col = color(140,60,90); // Central
+  d2col = color(330,70,100); // Ingleside
+  d3col = color(270,50,100); // Mission
   d4col = color(20,70,90); // Northern
   d5col = color(0,0,60); // Out of SF
   d6col = color(110,55,85); // Park
-  d7col = color(0,65,90); // Richmond
+  d7col = color(0,65,100); // Richmond
   d8col = color(210,20,85); // Southern
   d9col = color(50,60,85); // Taraval
-  d10col = color(250,60,90); // Tenderloin
+  d10col = color(250,60,100); // Tenderloin
 
   isVersionOne = true;
 
